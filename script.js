c54f165a6965c975f2d6fd1dc2225800881e2da1
akryvanych <56234383+akryvanych@users.noreply.github.com>
2019-12-05 14:28:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// var scene = document.getElementById('scene');
-// var parallaxInstance = new Parallax(scene);
-$('.parallax-window').parallax({imageSrc: 'img/fog-3622519_1920.jpg'});
-
 var myNav = document.getElementById("nav");
 var myNav = $("#nav");
 
@@ -13,22 +9,6 @@ $(window).on('scroll', function() {
     myNav.removeClass("scroll");
   }
 });
-
-// $(".carousel").on("touchstart", function(event){
-//   var xClick = event.originalEvent.touches[0].pageX;
-// $(this).one("touchmove", function(event){
-//   var xMove = event.originalEvent.touches[0].pageX;
-//   if( Math.floor(xClick - xMove) > 5 ){
-//       $(this).carousel('next');
-//   }
-//   else if( Math.floor(xClick - xMove) < -5 ){
-//       $(this).carousel('prev');
-//   }
-// });
-// $(".carousel").on("touchend", function(){
-//       $(this).off("touchmove");
-// });
-// });
 
 $("#myCarousel").swipe( {
   swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
